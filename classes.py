efa7c8edb22e691a317e65c6e57850fe6aaaebc9
Carson Katri <carson.katri@gmail.com>
2022-09-11 17:17:17 -0400

    Add troubleshooting section and improve menu UI

--- classes.py
+++ classes.py
@@ -1,11 +1,14 @@
 from .operators.install_dependencies import InstallDependencies
 from .operators.open_latest_version import OpenLatestVersion
 from .operators.dream_texture import DreamTexture
-from .preferences import OpenGitDownloads, OpenHuggingFace, OpenWeightsDirectory, ValidateInstallation, StableDiffusionPreferences
+from .operators.help_panel import HelpPanel
+
+from .preferences import OpenGitDownloads, OpenHuggingFace, OpenWeightsDirectory, OpenRustInstaller, ValidateInstallation, StableDiffusionPreferences
 from .shader_menu import ShaderMenu
 
 CLASSES = (
     DreamTexture,
+    HelpPanel,
     OpenLatestVersion,
     ShaderMenu,
 )
@@ -14,5 +17,6 @@ PREFERENCE_CLASSES = (OpenGitDownloads,
                       InstallDependencies,
                       OpenHuggingFace,
                       OpenWeightsDirectory,
+                      OpenRustInstaller,
                       ValidateInstallation,
                       StableDiffusionPreferences)
--- shader_menu.py
+++ shader_menu.py
@@ -1,6 +1,7 @@
 import bpy
 from .operators.dream_texture import DreamTexture
 from .operators.open_latest_version import OpenLatestVersion, new_version_available
+from .operators.help_panel import HelpPanel
 
 class ShaderMenu(bpy.types.Menu):
     bl_idname = "OBJECT_MT_dream_textures"
@@ -9,9 +10,10 @@ class ShaderMenu(bpy.types.Menu):
     def draw(self, context):
         layout = self.layout
 
-        layout.operator(DreamTexture.bl_idname)
+        layout.operator(DreamTexture.bl_idname, icon="IMAGE")
+        layout.operator(HelpPanel.bl_idname, icon="QUESTION")
         if new_version_available():
-            layout.operator(OpenLatestVersion.bl_idname)
+            layout.operator(OpenLatestVersion.bl_idname, icon="IMPORT")
 
 def shader_menu_draw(self, context):
     self.layout.menu(ShaderMenu.bl_idname)

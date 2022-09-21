2753aa9f17ecb4e8e3db12c9382939e85cd984ad
Carson Katri <carson.katri@gmail.com>
2022-09-21 19:56:37 -0400

    Add simple progress indicator

--- classes.py
+++ classes.py
@@ -1,21 +1,25 @@
 from .operators.install_dependencies import InstallDependencies
 from .operators.open_latest_version import OpenLatestVersion
-from .operators.dream_texture import DreamTexture
+from .operators.dream_texture import DreamTexture, ReleaseGenerator
 from .operators.help_panel import HelpPanel
 from .operators.view_history import RecallHistoryEntry, SCENE_UL_HistoryList, ViewHistory
 from .operators.inpaint_area_brush import InpaintAreaStroke
 from .property_groups.dream_prompt import DreamPrompt
+from .ui.panel import DREAM_PT_dream_panel, DREAM_PT_dream_node_panel
 
 from .preferences import OpenGitDownloads, OpenHuggingFace, OpenWeightsDirectory, OpenRustInstaller, ValidateInstallation, StableDiffusionPreferences
 
 CLASSES = (
     DreamTexture,
+    ReleaseGenerator,
     HelpPanel,
     OpenLatestVersion,
     ViewHistory,
     RecallHistoryEntry,
     InpaintAreaStroke,
     SCENE_UL_HistoryList,
+    DREAM_PT_dream_panel,
+    DREAM_PT_dream_node_panel,
 )
 
 PREFERENCE_CLASSES = (

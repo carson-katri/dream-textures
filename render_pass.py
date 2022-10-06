1edac76323d4b8bf6e0d78841ee513368c811239
Carson Katri <Carson.katri@gmail.com>
2022-10-05 20:02:37 -0400

    Add "Dream Textures" render pass to Cycles (#229)
    * Add base RenderPropertiesPanel
    
    * Working dream textures render pass
    
    * Revised implementation (with memory leak)
    
    * Fix step count
    
    * Refactor into subpanels
    
    * Remove Troubleshooting section
    
    * Use file picker to copy weights instead of manual installation
    
    * Replace Real-ESRGAN setup with ImportHelper
    
    * Add soft_min/soft_max to simulate exclusive clamped values
    
    * Add history management options and improved interface
    
    * Add prompt import/export
    
    * Fix seed export
    
    * Update submodules to fix inpainting
    
    * Updated to work with latest UI and internal changes
    
    * Remove unused handler
    
    * Remove completed TODO
    
    * Allow render pass properties to be animated
    
    * Update to latest submodules
    
    * Fix step count target when init image is specified
    
    * Add multiple of 64 check to size_x/size_y
    
    * Add closest hint to error
    
    Co-authored-by: NullSenseStudio <47096043+NullSenseStudio@users.noreply.github.com>
    
    Co-authored-by: NullSenseStudio <47096043+NullSenseStudio@users.noreply.github.com>

--- render_pass.py
+++ render_pass.py
@@ -1,20 +1,15 @@
 import bpy
 import cycles
-import numpy as np
-import os
-from .generator_process.actions.prompt_to_image import Pipeline, StepPreviewMode, ImageGenerationResult
-from .generator_process import Generator
 import threading
+import functools
+import numpy as np
+from multiprocessing.shared_memory import SharedMemory
 
-pass_inputs = [
-    ('color', 'Color', 'Provide the scene color as input'),
-    ('depth', 'Depth', 'Provide the Z pass as depth input'),
-    ('color_depth', 'Color and Depth', 'Provide the scene color and depth as input'),
-]
+from .operators.dream_texture import dream_texture
 
 update_render_passes_original = cycles.CyclesRender.update_render_passes
 render_original = cycles.CyclesRender.render
-# del_original = cycles.CyclesRender.__del__
+del_original = cycles.CyclesRender.__del__
 
 def register_render_pass():
     def update_render_passes_decorator(original):
@@ -40,22 +35,57 @@ def register_render_pass():
                     self.report({"ERROR"}, f"Image dimensions must be multiples of 64 (e.x. 512x512, 512x768, ...) closest is {round(size_x/64)*64}x{round(size_y/64)*64}")
                     return result
                 render_result = self.begin_result(0, 0, size_x, size_y)
-                for layer in render_result.layers:
-                    for render_pass in layer.passes:
+                for original_layer in original_result.layers:
+                    layer = None
+                    for layer_i in render_result.layers:
+                        if layer_i.name == original_layer.name:
+                            layer = layer_i
+                    for original_render_pass in original_layer.passes:
+                        render_pass = None
+                        for pass_i in layer.passes:
+                            if pass_i.name == original_render_pass.name:
+                                render_pass = pass_i
                         if render_pass.name == "Dream Textures":
-                            try:
-                                self._render_dream_textures_pass(layer, (size_x, size_y), scene, render_pass, render_result)
-                            except Exception as e:
-                                self.error_set(str(e))
+                            self.update_stats("Dream Textures", "Starting")
+                            def image_callback(event, shared_memory_name, seed, width, height, upscaled=False):
+                                self.update_stats("Dream Textures", "Pushing to render pass")
+                                # Only use the non-upscaled texture, as upscaling is currently unsupported by the addon.
+                                if not upscaled:
+                                    shared_memory = SharedMemory(shared_memory_name)
+                                    pixels = np.frombuffer(shared_memory.buf, dtype=np.float32)
+                                    reshaped = pixels.reshape((width * height, 4))
+                                    render_pass.rect.foreach_set(reshaped)
+                                    shared_memory.close()
+                                    event.set()
+                            
+                            step_count = int(scene.dream_textures_render_properties_prompt.strength * scene.dream_textures_render_properties_prompt.steps)
+                            def step_callback(step, width=None, height=None, shared_memory_name=None):
+                                self.update_stats("Dream Textures", f"Step {step + 1}/{step_count}")
+                                self.update_progress(step / step_count)
+                                return
+                            
+                            self.update_stats("Dream Textures", "Creating temporary image")
+                            combined_pass_image = bpy.data.images.new("dream_textures_post_processing_temp", width=size_x, height=size_y)
+                            
+                            rect = layer.passes["Combined"].rect
+                            
+                            combined_pixels = np.empty((size_x * size_y, 4), dtype=np.float32)
+                            rect.foreach_get(combined_pixels)
+                            combined_pass_image.pixels[:] = combined_pixels.ravel()
+
+                            self.update_stats("Dream Textures", "Starting...")
+                            event = threading.Event()
+                            def do_dream_texture_pass():
+                                dream_texture(scene.dream_textures_render_properties_prompt, step_callback, functools.partial(image_callback, event), combined_pass_image, width=size_x, height=size_y, show_steps=False)
+                            bpy.app.timers.register(do_dream_texture_pass)
+                            event.wait()
+                            def cleanup():
+                                bpy.data.images.remove(combined_pass_image)
+                            bpy.app.timers.register(cleanup)
+                            self.update_stats("Dream Textures", "Finished")
                         else:
-                            source_pass = None
-                            for original_layer in original_result.layers:
-                                if layer.name == original_layer.name:
-                                    for original_pass in original_layer.passes:
-                                        if original_pass.name == render_pass.name:
-                                            source_pass = original_pass
-                            pixels = np.empty((len(source_pass.rect), len(source_pass.rect[0])), dtype=np.float32)
-                            source_pass.rect.foreach_get(pixels)
+                            pixels = np.empty((size_x * size_y, 4), dtype=np.float32)
+                            original_render_pass.rect.foreach_get(pixels)
                             render_pass.rect[:] = pixels
                 self.end_result(render_result)
             except Exception as e:
@@ -63,7 +93,6 @@ def register_render_pass():
             return result
         return render
     cycles.CyclesRender.render = render_decorator(cycles.CyclesRender.render)
-    cycles.CyclesRender._render_dream_textures_pass = _render_dream_textures_pass
 
     # def del_decorator(original):
     #     def del_patch(self):
@@ -78,95 +107,5 @@ def unregister_render_pass():
     cycles.CyclesRender.update_render_passes = update_render_passes_original
     global render_original
     cycles.CyclesRender.render = render_original
-    del cycles.CyclesRender._render_dream_textures_pass
     # global del_original
     # cycles.CyclesRender.__del__ = del_original
-
-def _render_dream_textures_pass(self, layer, size, scene, render_pass, render_result):
-    self.update_stats("Dream Textures", "Starting")
-    
-    rect = layer.passes["Combined"].rect
-
-    match scene.dream_textures_render_properties_pass_inputs:
-        case 'color': pass
-        case 'depth' | 'color_depth':
-            depth = np.empty((size[0] * size[1], 1), dtype=np.float32)
-            layer.passes["Depth"].rect.foreach_get(depth)
-            depth = (1 - np.interp(depth, [0, np.ma.masked_equal(depth, depth.max(), copy=False).max()], [0, 1])).reshape((size[1], size[0]))
-    
-    combined_pixels = np.empty((size[0] * size[1], 4), dtype=np.float32)
-    rect.foreach_get(combined_pixels)
-
-    gen = Generator.shared()
-    self.update_stats("Dream Textures", "Applying color management transforms")
-    combined_pixels = gen.ocio_transform(
-        combined_pixels,
-        config_path=os.path.join(bpy.utils.resource_path('LOCAL'), 'datafiles/colormanagement/config.ocio'),
-        exposure=scene.view_settings.exposure,
-        gamma=scene.view_settings.gamma,
-        view_transform=scene.view_settings.view_transform,
-        display_device=scene.display_settings.display_device,
-        look=scene.view_settings.look,
-        inverse=False
-    ).result()
-
-    self.update_stats("Dream Textures", "Generating...")
-    
-    generated_args = scene.dream_textures_render_properties_prompt.generate_args()
-    generated_args['width'] = size[0]
-    generated_args['height'] = size[1]
-    match scene.dream_textures_render_properties_pass_inputs:
-        case 'color':
-            f = gen.image_to_image(
-                image=np.flipud(combined_pixels.reshape((size[1], size[0], 4)) * 255).astype(np.uint8),
-                **generated_args
-            )
-        case 'depth':
-            f = gen.depth_to_image(
-                depth=depth,
-                image=None,
-                **generated_args
-            )
-        case 'color_depth':
-            f = gen.depth_to_image(
-                depth=depth,
-                image=np.flipud(combined_pixels.reshape((size[1], size[0], 4)) * 255).astype(np.uint8),
-                **generated_args
-            )
-    event = threading.Event()
-    def on_step(_, step: ImageGenerationResult):
-        if step.final:
-            return
-        self.update_progress(step.step / generated_args['steps'])
-        if step.image is not None:
-            combined_pixels = step.image
-            render_pass.rect.foreach_set(combined_pixels.reshape((size[0] * size[1], 4)))
-            self.update_result(render_result) # This does not seem to have an effect.
-    def on_done(future):
-        nonlocal combined_pixels
-        result = future.result()
-        if isinstance(result, list):
-            result = result[-1]
-        combined_pixels = result.image
-        event.set()
-    f.add_response_callback(on_step)
-    f.add_done_callback(on_done)
-    event.wait()
-
-    # Perform an inverse transform so when Blender applies its transform everything looks correct.
-    self.update_stats("Dream Textures", "Applying inverse color management transforms")
-    combined_pixels = gen.ocio_transform(
-        combined_pixels.reshape((size[0] * size[1], 4)),
-        config_path=os.path.join(bpy.utils.resource_path('LOCAL'), 'datafiles/colormanagement/config.ocio'),
-        exposure=scene.view_settings.exposure,
-        gamma=scene.view_settings.gamma,
-        view_transform=scene.view_settings.view_transform,
-        display_device=scene.display_settings.display_device,
-        look=scene.view_settings.look,
-        inverse=True
-    ).result()
-
-    combined_pixels = combined_pixels.reshape((size[0] * size[1], 4))
-    render_pass.rect.foreach_set(combined_pixels)
-
-    self.update_stats("Dream Textures", "Finished")
--- ui/panels/render_properties.py
+++ ui/panels/render_properties.py
@@ -1,9 +1,5 @@
 import bpy
 from .dream_texture import create_panel, prompt_panel, advanced_panel
-from ...property_groups.dream_prompt import pipeline_options
-from ...generator_process.actions.prompt_to_image import Pipeline
-from ...generator_process.actions.huggingface_hub import ModelType
-from ...preferences import StableDiffusionPreferences
 
 class RenderPropertiesPanel(bpy.types.Panel):
     """Panel for Dream Textures render properties"""
@@ -27,33 +23,7 @@ class RenderPropertiesPanel(bpy.types.Panel):
         layout.use_property_decorate = False
         layout.active = context.scene.dream_textures_render_properties_enabled
 
-        if len(pipeline_options(self, context)) > 1:
-            layout.prop(context.scene.dream_textures_render_properties_prompt, "pipeline")
-        if Pipeline[context.scene.dream_textures_render_properties_prompt.pipeline].model():
-            layout.prop(context.scene.dream_textures_render_properties_prompt, 'model')
         layout.prop(context.scene.dream_textures_render_properties_prompt, "strength")
-        layout.prop(context.scene, "dream_textures_render_properties_pass_inputs")
-        if context.scene.dream_textures_render_properties_pass_inputs != 'color':
-            if not bpy.context.view_layer.use_pass_z:
-                box = layout.box()
-                box.label(text="Z Pass Disabled", icon="ERROR")
-                box.label(text="Enable the Z pass to use depth pass inputs")
-                box.use_property_split = False
-                box.prop(context.view_layer, "use_pass_z")
-
-            if not Pipeline[context.scene.dream_textures_render_properties_prompt.pipeline].depth():
-                box = layout.box()
-                box.label(text="Unsupported pipeline", icon="ERROR")
-                box.label(text="The selected pipeline does not support depth to image.")
-            
-            models = list(filter(
-                lambda m: m.model == context.scene.dream_textures_render_properties_prompt.model,
-                context.preferences.addons[StableDiffusionPreferences.bl_idname].preferences.installed_models
-            ))
-            if len(models) > 0 and ModelType[models[0].model_type] != ModelType.DEPTH:
-                box = layout.box()
-                box.label(text="Unsupported model", icon="ERROR")
-                box.label(text="Select a depth model, such as 'stabilityai/stable-diffusion-2-depth'")
 
 def render_properties_panels():
     yield RenderPropertiesPanel
@@ -63,7 +33,7 @@ def render_properties_panels():
     region_type = RenderPropertiesPanel.bl_region_type
     panels = [
         *create_panel(space_type, region_type, RenderPropertiesPanel.bl_idname, prompt_panel, get_prompt, True),
-        *create_panel(space_type, region_type, RenderPropertiesPanel.bl_idname, advanced_panel, get_prompt, True),
+        create_panel(space_type, region_type, RenderPropertiesPanel.bl_idname, advanced_panel, get_prompt, True),
     ]
     for panel in panels:
         def draw_decorator(original):

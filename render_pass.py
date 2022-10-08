f1c3d7261f0d55c5e1ddec8e9c8bbc8a0389e2b6
Carson Katri <carson.katri@gmail.com>
2022-10-08 19:13:40 -0400

    Use OpenColorIO to fix color management in render pass

--- render_pass.py
+++ render_pass.py
@@ -3,6 +3,10 @@ import cycles
 import threading
 import functools
 import numpy as np
+import math
+import os
+import sys
+import PyOpenColorIO as OCIO
 from multiprocessing.shared_memory import SharedMemory
 
 from .operators.dream_texture import dream_texture
@@ -53,7 +57,7 @@ def register_render_pass():
                                 if not upscaled:
                                     shared_memory = SharedMemory(shared_memory_name)
                                     pixels = np.frombuffer(shared_memory.buf, dtype=np.float32)
-                                    reshaped = pixels.reshape((width * height, 4))
+                                    reshaped = np.power(pixels.reshape((width * height, 4)), 2.4)
                                     render_pass.rect.foreach_set(reshaped)
                                     shared_memory.close()
                                     event.set()
@@ -71,7 +75,90 @@ def register_render_pass():
                             
                             combined_pixels = np.empty((size_x * size_y, 4), dtype=np.float32)
                             rect.foreach_get(combined_pixels)
-                            combined_pass_image.pixels[:] = combined_pixels.ravel()
+
+                            ocio_config = OCIO.Config.CreateFromFile(os.path.join(bpy.utils.resource_path('LOCAL'), 'datafiles/colormanagement/config.ocio'))
+                            
+                            # A reimplementation of `OCIOImpl::createDisplayProcessor` from the Blender source.
+                            # https://github.com/dfelinto/blender/blob/87a0770bb969ce37d9a41a04c1658ea09c63933a/intern/opencolorio/ocio_impl.cc#L643
+                            def create_display_processor(
+                                config,
+                                input_colorspace,
+                                view,
+                                display,
+                                look,
+                                scale, # Exposure
+                                exponent # Gamma
+                            ):
+                                group = OCIO.GroupTransform()
+
+                                # Exposure
+                                if scale != 1:
+                                    # Always apply exposure in scene linear.
+                                    color_space_transform = OCIO.ColorSpaceTransform()
+                                    color_space_transform.setSrc(input_colorspace)
+                                    color_space_transform.setDst(OCIO.ROLE_SCENE_LINEAR)
+                                    group.appendTransform(color_space_transform)
+
+                                    # Make further transforms aware of the color space change
+                                    input_colorspace = OCIO.ROLE_SCENE_LINEAR
+
+                                    # Apply scale
+                                    matrix_transform = OCIO.MatrixTransform([scale, 0.0, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, 0.0, 1.0])
+                                    group.appendTransform(matrix_transform)
+                                
+                                # Add look transform
+                                use_look = look is not None and len(look) > 0
+                                if use_look:
+                                    look_output = config.getLook(look).getProcessSpace()
+                                    if look_output is not None and len(look_output) > 0:
+                                        look_transform = OCIO.LookTransform()
+                                        look_transform.setSrc(input_colorspace)
+                                        look_transform.setDst(look_output)
+                                        look_transform.setLooks(look)
+                                        group.appendTransform(look_transform)
+                                        # Make further transforms aware of the color space change.
+                                        input_colorspace = look_output
+                                    else:
+                                        # For empty looks, no output color space is returned.
+                                        use_look = False
+                                
+                                # Add view and display transform
+                                print(input_colorspace)
+                                print(use_look)
+                                print(view)
+                                print(display)
+                                display_view_transform = OCIO.DisplayViewTransform()
+                                display_view_transform.setSrc(input_colorspace)
+                                display_view_transform.setLooksBypass(True)
+                                display_view_transform.setView(view)
+                                display_view_transform.setDisplay(display)
+                                group.appendTransform(display_view_transform)
+
+                                # Gamma
+                                if exponent != 1:
+                                    exponent_transform = OCIO.ExponentTransform([exponent, exponent, exponent, 1.0])
+                                    group.appendTransform(exponent_transform)
+                                
+                                # Create processor from transform. This is the moment were OCIO validates
+                                # the entire transform, no need to check for the validity of inputs above.
+                                try:
+                                    processor = config.getProcessor(group)
+                                    if processor is not None:
+                                        return processor
+                                except Exception as e:
+                                    print(e)
+                                
+                                return None
+                            
+                            # Exposure and gamma transformations derived from Blender source:
+                            # https://github.com/dfelinto/blender/blob/87a0770bb969ce37d9a41a04c1658ea09c63933a/source/blender/imbuf/intern/colormanagement.c#L825
+                            scale = 1 if scene.view_settings.exposure == 0 else math.pow(2, scene.view_settings.exposure)
+                            exponent = 1 if scene.view_settings.gamma == 1 else (1 / (scene.view_settings.gamma if scene.view_settings.gamma > sys.float_info.epsilon else sys.float_info.epsilon))
+                            processor = create_display_processor(ocio_config, OCIO.ROLE_SCENE_LINEAR, scene.view_settings.view_transform, scene.display_settings.display_device, scene.view_settings.look if scene.view_settings.look != 'None' else None, scale, exponent)
+
+                            flat_pixels = combined_pixels.ravel()
+                            processor.getDefaultCPUProcessor().applyRGBA(flat_pixels)
+                            combined_pass_image.pixels[:] = flat_pixels
 
                             self.update_stats("Dream Textures", "Starting...")
                             event = threading.Event()

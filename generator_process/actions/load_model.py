19e3e6616d7aa340346aa07826c32c54de8a4c66
NullSenseStudio <47096043+NullSenseStudio@users.noreply.github.com>
2023-08-19 22:25:29 -0400

    legacy fp16 revision loading

--- generator_process/actions/load_model.py
+++ generator_process/actions/load_model.py
@@ -10,21 +10,34 @@ def load_model(self, model_class, model, half_precision, **kwargs):
 
     if not hasattr(self, "_pipe") or self._pipe is None or self._pipe[0] != model:
         dtype = torch.float16 if half_precision else torch.float32
-        variant = "fp16" if half_precision else None
         model_path_or_repo = model
-        if "/" not in model_path_or_repo:
+        is_repo = "/" in model_path_or_repo
+        if os.path.exists(os.path.join(model, "model_index.json")):
+            is_repo = False
+        elif not is_repo and os.path.exists(os.path.join(DIFFUSERS_CACHE, model, "model_index.json")):
             model_path_or_repo = os.path.join(DIFFUSERS_CACHE, model)
-            if not os.path.isdir(model_path_or_repo):
-                raise ValueError(f"{model} is not a valid repo or imported checkpoint")
+        elif not is_repo:
+            raise ValueError(f"{model} is not a valid repo, imported checkpoint, or path")
 
-        try:
-            self._pipe = (model, model_class.from_pretrained(model_path_or_repo, torch_dtype=dtype, variant=variant, local_files_only=True, **kwargs))
-        except OSError:
+        strategies = [{"variant": "fp16" if half_precision else None}]
+        if not half_precision:
             # fp16 variant can automatically use fp32 files, but fp32 won't automatically use fp16 files
-            if half_precision:
-                raise
-            logger.warning(f"Can't load fp32 weights for model {model}, attempting to load fp16 instead")
-            self._pipe = (model, model_class.from_pretrained(model_path_or_repo, torch_dtype=dtype, variant="fp16", local_files_only=True, **kwargs))
+            strategies.append({"variant": "fp16", "_warn_precision_fallback": True})
+        if is_repo and os.path.exists(os.path.join(DIFFUSERS_CACHE, "--".join(["models", *model.split("/")]), "refs", "fp16")):
+            strategies.append({"revision": "fp16"})
+
+        def try_strategies():
+            exc = None
+            for strat in strategies:
+                if strat.pop("_warn_precision_fallback", False):
+                    logger.warning(f"Can't load fp32 weights for model {model}, attempting to load fp16 instead")
+                try:
+                    return model_class.from_pretrained(model_path_or_repo, torch_dtype=dtype, local_files_only=True, **strat, **kwargs)
+                except Exception as e:
+                    if exc is None:
+                        exc = e
+            raise exc
+        self._pipe = (model, try_strategies())
     elif model_class.__name__ not in {
         # some tasks are not support by auto pipeline
         'DreamTexturesDepth2ImgPipeline'

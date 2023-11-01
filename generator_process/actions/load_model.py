243ce77a4ae55adddc622d4e99dad8bd6a08d522
Carson Katri <carson.katri@gmail.com>
2023-10-31 21:47:01 -0400

    Add workaround for AutoPipeline.from_pipe with ControlNet

--- generator_process/actions/load_model.py
+++ generator_process/actions/load_model.py
@@ -137,8 +137,8 @@ def _convert_pipe(cache, model, pipe, model_class, half_precision, scheduler, **
     if model_class.__name__ not in {
         # some tasks are not supported by auto pipeline
         'DreamTexturesDepth2ImgPipeline',
-        'StableDiffusionUpscalePipeline'
-    }:
+        'StableDiffusionUpscalePipeline',
+    } and not 'controlnet' in kwargs: 
         pipe = model_class.from_pipe(pipe, **kwargs)
     scheduler.create(pipe)
     return pipe

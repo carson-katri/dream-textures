5d255de9adb1b0eec8a7de8272763808080a041e
Carson Katri <carson.katri@gmail.com>
2023-12-22 17:31:23 -0500

    Reduce CFG Scale minimum to 0 to disable

--- property_groups/dream_prompt.py
+++ property_groups/dream_prompt.py
@@ -125,7 +125,7 @@ attributes = {
     "seed": StringProperty(name="Seed", default="0", description="Manually pick a seed", update=seed_clamp),
     "iterations": IntProperty(name="Iterations", default=1, min=1, description="How many images to generate"),
     "steps": IntProperty(name="Steps", default=25, min=1),
-    "cfg_scale": FloatProperty(name="CFG Scale", default=7.5, min=1, soft_min=1.01, description="How strongly the prompt influences the image"),
+    "cfg_scale": FloatProperty(name="CFG Scale", default=7.5, min=0, description="How strongly the prompt influences the image"),
     "scheduler": EnumProperty(name="Scheduler", items=scheduler_options, default=3), # defaults to "DPM Solver Multistep"
     "step_preview_mode": EnumProperty(name="Step Preview", description="Displays intermediate steps in the Image Viewer. Disabling can speed up generation", items=step_preview_mode_options, default=1),
 
